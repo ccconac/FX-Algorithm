f498166a03d6eb19a7ee43c9d3ac4984c7157a04
ccconac <qwerpoiu91@naver.com>
2024-01-21 20:21:26 +0900

    [BOJ] 알고리즘 수업 - 깊이 우선 탐색 4

---
+++ 3.DFS/BFS/[BOJ]/(24482번)알고리즘 수업 - 깊이 우선 탐색 4/[sohee] solution.js
@@ -0,0 +1,31 @@
+const input = require('fs')
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => v.split(' ').map(Number));
+
+const [N, M, R] = input.shift();
+const graph = [...Array(N + 1)].map(() => []);
+const visited = Array(N).fill(-1);
+
+input.map(([from, to]) => {
+  graph[from].push(to);
+  graph[to].push(from);
+});
+
+graph.map((v) => v.sort((a, b) => b - a));
+
+let count = 0;
+
+const DFS = (node, depth) => {
+  if (visited[node - 1] === -1) {
+    visited[node - 1] = depth;
+    for (let next of graph[node]) {
+      DFS(next, depth + 1);
+    }
+  }
+};
+
+DFS(R, count);
+console.log(visited.join('\n'));
